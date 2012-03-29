6cc37f037086a4d9c1785c96d4bda914e18fde18
Christian Pfandler <christian@pfandler.com>
2012-03-29 15:28:17 +0200

    modified lines

--- Core/Source/NSString+MobFox.h
+++ Core/Source/NSString+MobFox.h
@@ -3,7 +3,6 @@
 //
 //  Created by Oliver Drobnik on 9/24/10.
 //  Copyright 2010 Drobnik.com. All rights reserved.
-//  Modified by Michael Kaye sendmetospace.co.uk
 
 #import <Foundation/Foundation.h>
 
